e262c3502a003717b357e1b4a9090be67719a437
Jim MacKenzie <protojazz@gmail.com>
2017-01-05 22:05:16 -0600

    Add project files.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿ 
+function startup () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = 'Jim MacKenzie';
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function() {
+        main.style.backgroundColor = "#888";
+    };
+    main.onmouseleave = function() {
+        main.style.backgroundColor = "";
+    };
+}
+
+startup();
