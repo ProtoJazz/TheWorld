91a9e53addf5eb99be2328c996654f85886c7fb1
Jim MacKenzie <protojazz@gmail.com>
2017-01-14 22:26:48 -0600

    Up to API

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using TheWorld.Models;
 using TheWorld.Services;
 using TheWorld.ViewModels;
@@ -16,18 +17,28 @@ namespace TheWorld.Controllers.Web
 
         private IMailService _mailService;
         private IConfigurationRoot _config;
-        private WorldContext _context;
+        private IWorldRepository _repository;
+        private ILogger<AppController> _logger;
 
-        public AppController(IMailService mailService, IConfigurationRoot config, WorldContext context)
+        public AppController(IMailService mailService, IConfigurationRoot config, IWorldRepository repository, ILogger<AppController> logger)
         {
             _config = config;
             _mailService = mailService;
-            _context = context;
+            _repository = repository;
+            _logger = logger;
         }
         public IActionResult Index()
         {
-            var data = _context.Trips.ToList();
-            return View();
+            try
+            {
+                var data = _repository.GetAllTrips();
+                return View(data);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get trips in Index page : {ex.Message}");
+                return Redirect("/error");
+            }
         }
 
         public IActionResult Contact()
